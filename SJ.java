6a9e9ec6c88bd623c91aa66cca6670c85e476103
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-01-29 12:24:03 +0530

    taking argument to run code

---
+++ SJ.java
@@ -0,0 +1,8 @@
+class SJ{
+ public static void main(String[] args){
+  System.out.println("Rajan Yadav");
+  System.out.println("Student at LPU");
+  System.out.println(args[0]);
+  System.out.println(args[1]);
+ }
+}
