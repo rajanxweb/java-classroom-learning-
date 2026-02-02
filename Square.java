c8105e5230855ebf5187cf341e9e039ac747f8e6
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-02-02 10:56:52 +0530

    practice problem

---
+++ Square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        double val = Math.sqrt((b*b)-(4*a*c));
+        if(val>0){
+            System.out.println("Roots are real and distinct");
+        }
+        else if(val ==0){
+            System.out.println("Roots are real and equal");
+        }
+        else{
+            System.out.println("Roots are imaginary");
+        }
+    }
+}
