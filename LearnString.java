8b362134c9fa9bb892f5ab2ef859a8f2f03bd4ec
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-02-09 10:57:31 +0530

    string in java

---
+++ LearnString.java
@@ -0,0 +1,27 @@
+public class LearnString {
+    public static void main(String[] args){
+        // char S[] = {'j','a','v','a'};
+        // String t1 = new String(S);
+        // String t2 = new String(S,1,2);
+        // System.out.println(t1);
+        // System.out.println(t1.length());
+        // System.out.println(t2);
+        // System.out.println(t2.length());
+
+
+        String str = new String("java Programming");
+        String str2 = new String("cpp Programming");
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(8));
+        System.out.println(str.equals(str2));
+        System.out.println(str.toUpperCase());
+        
+
+
+        
+
+    }
+
+}
--- While.java
+++ While.java
@@ -2,23 +2,35 @@ import java.util.Scanner;
 
 public class While {
     static public void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int i = sc.nextInt();
 
-        //while loop
-        while(i<=10){
-            System.out.println(i);
-            i++;
-        }
-        //for loop
-        do{
-            System.out.println(i);
-            i--;
-        }while(i>0);
+        // //while loop
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // //do-while loop
+        // do{
+        //     System.out.println(i);
+        //     i--;
+        // }while(i>0);
 
-        //for loop
-        for(int x=0; x<i; x++){
-            System.out.println(x);
+        // //for loop
+        // for(int x=0; x<i; x++){
+        //     System.out.println(x);
+        // }
+
+        // //for-each loop
+        // int x[] ={1,2,3,4,5};
+        // for(int j:x){
+        //     System.out.println(j);
+        // }
+
+        String cityName[] = {"Mumbai", "Pune", "Hydrabad", "Goal", "Varanshi"};
+        for(String i:cityName)
+            {
+            System.out.println(i);
         }
 
     }
